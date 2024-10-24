d6ff0454c24c75510b99aa4aef41633df13bb02c
Rafael Riber <rafael@riber.ch>
2024-10-24 13:05:22 +0200

    Add Arty to targets

--- src/amaranth_template_fpga/__init__.py
+++ src/amaranth_template_fpga/__init__.py
@@ -3,6 +3,7 @@ from amaranth import *
 from amaranth_boards.icebreaker import ICEBreakerPlatform
 from amaranth_boards.versa_ecp5 import VersaECP5Platform
 from amaranth_boards.tang_nano import TangNanoPlatform
+from amaranth_boards.arty_a7 import ArtyA7_100Platform
 
 from .blinky import Blinky
 
@@ -27,3 +28,6 @@ def build_ecp5():
 
 def build_gowin():
     TangNanoPlatform().build(Toplevel())
+
+def build_arty100():
+    ArtyA7_100Platform().build(Toplevel())
